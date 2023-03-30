47486269a608949855765b2ac95650b31e9d1fac
Inkya-ha <admin@inkya-ha.com>
2023-03-31 05:36:23 +0900

    トップページのデザイン改装

--- index.php
+++ index.php
@@ -8,11 +8,21 @@
 
     <main>
         <div id="top">
-            <p class="fs-1">
-                FingerPartner
-            </p>
-            <div style="margin-bottom: 5vh;"><!--改行タグ--></div>
-            <hr>
+            <div class="background-design">
+                <img src="./image/background.svg" alt="FingerPartner">
+            </div>
+            <svg class="arrows">
+                <path class="a1" d="M0 0 L30 32 L60 0"></path>
+                <path class="a2" d="M0 20 L30 52 L60 20"></path>
+                <path class="a3" d="M0 40 L30 72 L60 40"></path>
+            </svg>
+        </div><div style="margin-top: 25vh;"></div>
+        <div id="content">
+            <div id="content-one">
+                <p class="fs-1">お客様に合った商品を</p>
+                <hr>
+                <p class="fs-6">お客様のご意見を真摯に受け止め、お客様の理想なオリジナルキーボードを製作します。</p>
+            </div>
         </div>
     </main>
 
