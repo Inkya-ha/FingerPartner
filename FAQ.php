7bf202c1b6163e221489e47fc5b35da6d20bc016
Inkya-ha <admin@inkya-ha.com>
2023-03-31 02:03:30 +0900

    よくある質問についてのページ記載 (完成)

---
+++ FAQ.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <?php include_once __DIR__ . "/static/head/head.php"; ?>
+    <title>Q&A｜FingerPartner</title>
+</head>
+<body>
+    <?php include_once __DIR__ . "/static/header/header.php"; ?>
+
+    <main>
+        <p class="fs-1 label1">よくある質問</p>
+        <div id="question" class="mg-3">
+            <dl class="qa-004">
+                <hr><dt>支払い方法にはどのようなものがありますか？</dt>
+                <dd>ご利用いただけるお支払い方法は以下の通りです。「クレジットカード、デビットカード、Apple Pay、Google Pay等」</dd><hr>
+                <dt>依頼から配送までの期間はどのくらいですか？</dt>
+                <dd>打ち合わせの期間や設計によって大きく左右されることがありますが、通常2ヶ月〜3ヶ月の間に納品を行います。</dd><hr>
+                <dt>支払い手順がわかりません。</dt>
+                <dd>商品の設計後、設計を確定しましたらメールまたはチャットツールで取引代行サービスのお支払いURLをお送りします。画面の案内に従ってお支払いを完了してください。</dd><hr>
+                <dt>請求書・受領書・見積書の発行は可能ですか？</dt>
+                <dd>いずれも発行可能です。打ち合わせ担当者に直接、必要書類の発行をして欲しいと言う旨を伝えてください。担当者が書類を作成します。</dd><hr>
+                <dt>依頼をしましたがキャンセルできますか？</dt>
+                <dd>依頼時のキャンセルはいつでも無償で可能ですが、設計を確定した後、商品の製作に取り掛かった時点でキャンセルは不可能となりますので、キャンセルを行いたい場合は担当スタッフまでお早めにお伝えください。</dd><hr>
+                
+            </dl>
+        </div>
+    </main>
+
+    <?php include_once __DIR__ . "/static/footer/footer.php"; ?>
+</body>
+</html>
