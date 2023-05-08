79d0255392fe788be8dc71dac74adf355e836e64
Inkya-ha <admin@inkya-ha.com>
2023-05-08 23:23:50 +0900

    調整

--- index.php
+++ index.php
@@ -13,6 +13,37 @@
             </div>
         </div>
         <div style="margin-top: 25vh;"><!--改行タグ--></div>
+        <div id="requests" class="requests-box">
+            <div id="title">
+                <p class="fs-1">キーボード製作依頼</p>
+            </div>
+            <div style="margin-top: 5vh;"><!--改行タグ--></div>
+            <div id="line">
+                <p class="fs-3">LINEでのご依頼はこちらから</p>
+                <a href="https://lin.ee/BPiGa2h" target="_BLANK"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"></a>
+                <div style="margin-top: 1vh;"><!--改行タグ--></div>
+                <p class="fs-6">LINE公式アカウントの友達追加を行い、トークにて「キーボードの製作依頼をしたい」と一言いただければ、スタッフが確認を行い、文面での打ち合わせを行います。お客様がご希望される場合につきましては、ZOOM等でビデオ通話ミーティングをすることができますので、担当スタッフにお伝えくださいませ。</p>
+            </div><div style="margin-top: 8vh;"><!--改行タグ--></div>
+            <div id="discord">
+                <p class="fs-3">Discordでのご依頼はこちらから</p>
+                <a href="https://discord.gg/taZqXsSQUY" target="_BLANK"><img src="./image/source/discord.svg" alt="Discord" height="36"></a>
+                <div style="margin-top: 1vh;"><!--改行タグ--></div>
+                <p class="fs-6">Discordの公式サーバーに参加し、必要な利用規約等の確認を済ませた後、「お問い合わせ」というテキストチャンネルから、キーボードの製作依頼をすることが可能です。</p>
+            </div><div style="margin-top: 8vh;"><!--改行タグ--></div>
+            <div id="mail">
+                <p class="fs-3">メールでのご依頼はこちらから</p>
+                <a href="./Contact" class="btn btn--orange btn--radius" target="_BLANK">メールでのご依頼</a>
+                <div style="margin-top: 1vh;"><!--改行タグ--></div>
+                <p class="fs-6">お問い合わせフォームより必須事項を記入した上、キーボードの製作依頼をすることを記入してください。スタッフが確認後メールで返信を行います。</p>
+            </div><div style="margin-top: 8vh;"><!--改行タグ--></div>
+            <div id="phone">
+                <p class="fs-3">電話でのご依頼はこちらから</p>
+                <a href="tel:050-6873-8346" class="btn btn--orange btn--radius">電話でのご依頼</a>
+                <div style="margin-top: 1vh;"><!--改行タグ--></div>
+                <p class="fs-6">「電話でのご依頼」をクリックしていただくと、当社電話番号が表示され、当社へ依頼の電話を行うことができます。キーボードに関しての打ち合わせはLINE公式アカウント又はDiscord・メールでの打ち合わせを予定しております。全てお電話で完結することはありません。予めご了承くださいませ。</p>
+            </div>
+        </div>
+        <div style="margin-top: 25vh;"><!--改行タグ--></div>
         <div id="content">
             <div id="content-one">
                 <p class="fs-1">お客様に合った商品を</p>
